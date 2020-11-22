33f6bdea15181a1cb2ab3eca71a03ca515673c7c
Ayush Kholiya <52167169+ayushk048@users.noreply.github.com>
2020-11-22 15:11:08 +0530

    Add files via upload

---
+++ precache-manifest.e2365413d3586d6274d7b93c5443622a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6655da7aa643d105807632ab89b521",
+    "url": "/ayushk048/bracking-bad/index.html"
+  },
+  {
+    "revision": "3106650e2349f12beb5f",
+    "url": "/ayushk048/bracking-bad/static/css/main.47ae3587.chunk.css"
+  },
+  {
+    "revision": "51ed7880973ac9989103",
+    "url": "/ayushk048/bracking-bad/static/js/2.dee9f4b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ayushk048/bracking-bad/static/js/2.dee9f4b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3106650e2349f12beb5f",
+    "url": "/ayushk048/bracking-bad/static/js/main.695b9fd1.chunk.js"
+  },
+  {
+    "revision": "d935cf30cd11b2f7454c",
+    "url": "/ayushk048/bracking-bad/static/js/runtime-main.34ba4561.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/ayushk048/bracking-bad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/ayushk048/bracking-bad/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/ayushk048/bracking-bad/static/media/spinner.235b9b3e.gif"
+  }
+]);
